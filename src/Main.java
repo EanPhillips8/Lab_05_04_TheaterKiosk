75ca603d5dbe6b15969620a055993525a7a51137
EanPhillips8 <eanphillips8@gmail.com>
2022-09-22 13:47:00 -0400

    j

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+     int age= 19;
+        System.out.println("What is your age: ");
+
+     if (age>= 21)
+            System.out.println("You are 21 or older, You get a wristband!");
+
+     else
+             System.out.println("Sorry you're not old enough, you can't get a wristband.");
+
     }
 }
